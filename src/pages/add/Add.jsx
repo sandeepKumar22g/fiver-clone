613fb4457606fbceaf64e8b244ef2dab8becc2e4
Sandeep <sandy6862g22@gmail.com>
2023-03-22 18:03:13 +0530

    intialized the pages folder

---
+++ src/pages/add/Add.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./add.scss"
+
+
+const Add = () => {
+  return (
+    <div>Add</div>
+  )
+}
+
+export default Add
---
+++ src/pages/myOrders/MyOrders.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./myorders.scss"
+
+const MyOrders = () => {
+  return (
+    <div>MyOrders</div>
+  )
+}
+
+export default MyOrders
