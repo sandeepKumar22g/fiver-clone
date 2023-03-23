c4e4ccb1be209e0e45e35168dbee0b9bee201ae1
Sandeep <sandy6862g22@gmail.com>
2023-03-23 17:54:27 +0530

    create a index file for the components and assets

---
+++ src/assets/index.js
@@ -0,0 +1,25 @@
+import AccessiblityImg from "./accessibility.png"
+import CheckImg from "./check.png"
+import ClockImg from "./clock.png"
+import CoinImg from "./coin.png"
+import DeleteImg from "./delete.png"
+import DislikeImg from "./dislike.png"
+import DownImg from "./down.png"
+import FacebookImg from "./facebook.png"
+import GreenCheckImg from "./greencheck.png"
+import HeartImg from "./heart.png"
+import InstagramImg from "./instagram.png"
+import LanguageImg from "./language.png"
+import LikeImg from "./like.png"
+import LinkedinImg from "./linkedin.png"
+import ManImg from "./man.png"
+import MessageImg from "./message.png"
+import PinterestImg from "./pinterest.png"
+import RecycleImg from "./recycle.png"
+import SearchImg from "./search.png"
+import StarImg from "./star.png"
+import TwitterImg from "./twitter.png"
+
+
+
+export {AccessiblityImg, CheckImg, ClockImg, CoinImg, DeleteImg, DislikeImg,DownImg, FacebookImg, GreenCheckImg, HeartImg, InstagramImg, LanguageImg, LikeImg, LinkedinImg, ManImg, MessageImg, PinterestImg, RecycleImg, SearchImg, StarImg, TwitterImg}
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Navbar from "./navbar/Navbar"
+import Footer from "./footer/Footer"
+import Featured from "./featured/Featured"
+import TrustedBy from "./trustedby/TrustedBy"
+
+export { Navbar, Featured, Footer, TrustedBy }
