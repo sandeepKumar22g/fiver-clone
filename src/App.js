e0327950add575128e341434e72c22df342b05c5
Sandeep <sandy6862g22@gmail.com>
2023-03-23 15:51:38 +0530

    working on navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,71 @@
 import './App.css';
 import React from 'react'
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import {Add,Gig, Gigs,Home,Login, Message, Messages,MyGigs,MyOrders,Register} from "./pages"
 
 function App() {
-  return (
-    <>
-    <h1>hello world</h1>
-    </>
-  );
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/myGigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/orders",
+          element: <MyOrders />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+      ],
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
