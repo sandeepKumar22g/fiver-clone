5cbc31fa7095931825ee51ace3f8b2f0fb049c0f
Sandeep <sandy6862g22@gmail.com>
2023-04-04 17:51:00 +0530

    create the gig or category page

---
+++ src/components/gigCard/GigCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import "./gigCard.scss";
+import { Link } from 'react-router-dom';
+import {StarImg, HeartImg} from "../../assets"
+
+const GigCard = ({item}) => {
+  return (
+    <Link to="/gig/123" className="link" style={{textDecoration:"none"}} >
+      <div className="gigCard">
+        <img src={item.img} alt="" />
+        <div className="info">
+          <div className="user">
+            <img src={item.pp} alt="" />
+            <span>{item.username}</span>
+          </div>
+          <p>{item.desc}</p>
+          <div className="star">
+            <img src={StarImg} alt="" />
+            <span>{item.star}</span>
+          </div>
+        </div>
+        <hr />
+        <div className="detail">
+          <img src={HeartImg} alt="" />
+          <div className="price">
+            <span>STARTING AT</span>
+            <h2>
+              $ {item.price}
+              <sup>99</sup>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default GigCard
